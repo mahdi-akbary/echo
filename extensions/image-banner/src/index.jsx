d925d0533b40af9193eb821123dbd0d1b85951cd
Hujjat <hujjatnazari@gmail.com>
2023-05-24 23:36:32 +1000

    Fixed bug on image banner

--- extensions/image-banner/src/index.jsx
+++ extensions/image-banner/src/index.jsx
@@ -13,24 +13,22 @@ function App() {
     fit,
     border,
     corner_radius,
-    aspect_ratio,
   } = useSettings();
 
-  const aspectRatio = aspect_ratio == "square" ? '1/1': aspect_ratio == "portrait" ? '3/4' : aspect_ratio == "landscape" ? '4/3' : '2/1';
 
   return (
     <>
       {image_url ? (
         <Image
           source={image_url}
-          accessibilityDescription="accessibility_description"
+          accessibilityDescription={accessibility_description}
           loading={lazyloading ? "lazy" : "eager"}
           fit={ fit ? 'cover' : "contain"}
           border={ border ? "base" : "none"}
           cornerRadius={corner_radius}
         />
       ) : (
-        <SkeletonImage inlineSize={300} blockSize={300} aspectRatio={aspectRatio} />
+        <SkeletonImage inlineSize={300} blockSize={300} />
       )}
     </>
   );

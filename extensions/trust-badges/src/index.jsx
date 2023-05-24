cea932cd4f9be14585c247cbe543c1b0a1e02923
Hujjat <hujjatnazari@gmail.com>
2023-05-24 12:35:23 +1000

    Trust badge with option done

--- extensions/trust-badges/src/index.jsx
+++ extensions/trust-badges/src/index.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
 import {
-  useExtensionApi,
   render,
-  Banner,
-  useTranslate,
-} from '@shopify/checkout-ui-extensions-react';
+  InlineLayout,
+  useSettings,
+  TextBlock,
+  View,
+  BlockSpacer,
+  Text,
+  Image,
+} from "@shopify/checkout-ui-extensions-react";
 
-render('Checkout::Dynamic::Render', () => <App />);
+render("Checkout::Dynamic::Render", () => <App />);
 
 function App() {
-  const {extensionPoint} = useExtensionApi();
-  const translate = useTranslate();
+  const { trust_title, trust_description, trust_icon } = useSettings();
   return (
-    <Banner title="Trust-badges">
-      {translate('welcome', {extensionPoint})}
-    </Banner>
+    <InlineLayout columns={["16%", "fill"]}>
+      <View border="none" padding="tight">
+        <Image source={trust_icon ?? "https://cdn.shopify.com/s/files/1/0725/8836/2008/files/trust-badge.png"} />
+      </View>
+
+      <View border="none" padding="tight">
+        <Text emphasis="bold">{trust_title ?? "Trusted by 1000s of customers"}</Text>
+        <BlockSpacer spacing="extraTight" />
+        <TextBlock size="small" appearance="subdued">
+          {trust_description ?? "We are a trusted store"}
+        </TextBlock>
+      </View>
+    </InlineLayout>
   );
 }

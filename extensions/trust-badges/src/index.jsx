2d524d984cabf4eafe867dd64390d61efd8e0336
Hujjat <hujjatnazari@gmail.com>
2023-05-24 12:58:30 +1000

    image width as choice option added

--- extensions/trust-badges/src/index.jsx
+++ extensions/trust-badges/src/index.jsx
@@ -12,9 +12,12 @@ import {
 render("Checkout::Dynamic::Render", () => <App />);
 
 function App() {
-  const { trust_title, trust_description, trust_icon } = useSettings();
+  const { trust_title, trust_description, trust_icon, image_width } = useSettings();
+
+  const imageWidth = image_width === "small" ? "16%" : image_width === "medium" ? "20%" : image_width === "large" ? "32%" : image_width === "extraLarge" ? "48%" : "16%";
+
   return (
-    <InlineLayout columns={["16%", "fill"]}>
+    <InlineLayout columns={[imageWidth, "fill"]}>
       <View border="none" padding="tight">
         <Image source={trust_icon ?? "https://cdn.shopify.com/s/files/1/0725/8836/2008/files/trust-badge.png"} />
       </View>

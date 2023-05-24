62bbc20afdb21d290141a3cbb808015697b3a375
Hujjat <hujjatnazari@gmail.com>
2023-05-24 14:55:53 +1000

    image banner minor updates

--- extensions/image-banner/src/index.jsx
+++ extensions/image-banner/src/index.jsx
@@ -1,18 +1,34 @@
-import { 
-  render, 
+import {
+  render,
   Image,
   useSettings,
   SkeletonImage,
-  } from "@shopify/checkout-ui-extensions-react";
+} from "@shopify/checkout-ui-extensions-react";
 render("Checkout::Dynamic::Render", () => <App />);
 function App() {
-  const { image_url } = useSettings();
+  const {
+    image_url,
+    accessibility_description,
+    lazyloading,
+    fit,
+    border,
+    corner_radius,
+  } = useSettings();
+
+
   return (
     <>
       {image_url ? (
-        <Image source={image_url} />
+        <Image
+          source={image_url}
+          accessibilityDescription="accessibility_description"
+          loading={lazyloading ? "lazy" : "eager"}
+          fit={ fit ? 'cover' : "contain"}
+          border={ border ? "base" : "none"}
+          cornerRadius={corner_radius}
+        />
       ) : (
-        <SkeletonImage inlineSize={300} blockSize={300}  />
+        <SkeletonImage inlineSize={300} blockSize={300} aspectRatio={1 / 2} />
       )}
     </>
   );

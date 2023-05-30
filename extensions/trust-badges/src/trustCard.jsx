9e5fcf26539632e7c934c7e3477e22c96752f56c
Hujjat <hujjatnazari@gmail.com>
2023-05-30 16:31:04 +1000

    border for trustcard updated

--- extensions/trust-badges/src/trustCard.jsx
+++ extensions/trust-badges/src/trustCard.jsx
@@ -14,16 +14,16 @@ import {
 export function TrustCard({ title, description, icon, width }) {
 
     let { border, padding, alignment } = useSettings();
-
     const imageWidth = width === "small" ? "16%" : width === "medium" ? "20%" : width === "large" ? "32%" : width === "extraLarge" ? "48%" : "16%";
-    
+    '
     return (
-        <InlineLayout columns={[imageWidth, "fill"]}>
-            <View border={border} padding="tight">
+
+        <InlineLayout columns={[imageWidth, "fill"]} padding={padding} border={border} cornerRadius={'base'}>
+            <View>
                 <Image accessibilityDescription={description} source={ icon ?? "https://cdn.shopify.com/s/files/1/0725/8836/2008/files/trust-badge.png"} />
             </View>
     
-            <View border={border} padding="tight">
+            <View padding="tight">
                 <TextBlock emphasis="bold" inlineAlignment={alignment}>{ title }</TextBlock>
                 <BlockSpacer spacing="extraTight" />
                 <TextBlock size="small" inlineAlignment={alignment} appearance="subdued">

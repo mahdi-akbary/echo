b3440eb25b67ab4bb454089bce464cb67070e173
Mahdi Nikzad <mahdi.akbary.nikzad@gmail.com>
2023-07-29 15:45:43 +0430

    adds the how to use text description

--- web/frontend/pages/how-to-use.jsx
+++ web/frontend/pages/how-to-use.jsx
@@ -11,7 +11,7 @@ import {
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useEffect, useState } from "react";
 
-export default function HowToUse () {
+export default function HowToUse() {
   const [width, setWidth] = useState(window.innerWidth);
   const updateDimensions = () => {
     setWidth(window.innerWidth);
@@ -24,52 +24,38 @@ export default function HowToUse () {
 
   const videoList = [
     {
-      title: "AI Product Recommendations",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
+      title: "AI Product Recommendation",
+      subTitle: `Personalize the shopping experience and maximize sales with AI-driven suggestions tailored to each customer's preferences.`,
       link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
     },
     {
       title: "Count Down Timer",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
+      subTitle: `Drive urgency and increase conversions by creating a time-sensitive atmosphere that compels customers to act quickly.`,
       link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
     },
     {
-      title: "Image banner",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
-      link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
-    },
-    {
-      title: "Custom Fields",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
-      link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
-    },
-    {
-      title: "Line Item Change",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
+      title: "Custom Image Banners",
+      subTitle: `Captivate shoppers with visually enticing banners showcasing promotions and special offers, enhancing engagement and product visibility.`,
       link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
     },
     {
       title: "Trust Badges",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
+      subTitle: `Instill confidence in customers by displaying trust badges, assuring them of a secure and trustworthy transaction process.`,
       link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
     },
     {
-      title: "Checkout Upsell",
-      subTitle: `
-          Use AI to recommend products to your customers. View
-          analytics to see how your recommendations are performing.`,
+      title: "Custom Fields in Checkout",
+      subTitle: `Gather valuable customer data with customizable fields, enabling targeted marketing and a deeper understanding of your audience.`,
+      link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
+    },
+    {
+      title: "Quantity Modifier",
+      subTitle: `Empower shoppers to easily adjust product quantities, streamlining the checkout process and improving overall satisfaction.`,
+      link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
+    },
+    {
+      title: "Checkout Free Gifts",
+      subTitle: `Delight customers with surprise rewards! Encourage higher spending and nurture loyalty by offering enticing free gifts upon reaching a spending threshold.`,
       link: "https://www.loom.com/embed/799f5eb9613c41b9a092664f0151d6e0?sid=5820c294-15fb-4628-83cb-b6da0ba8afa1?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true",
     },
   ];
@@ -114,14 +100,31 @@ export default function HowToUse () {
                   width={width < 780 ? "100%" : `${(width * 50) / 100}px`}
                   minHeight={`${(width * 33) / 100}px`}
                 >
-                  <div style={{ width: "100%", height: "100%", zIndex: 1, position: "absolute", display: 'flex', justifyContent: 'center', alignItems: "center", border: '1px solid #ccc', borderRadius: '5px' }}>
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      zIndex: 1,
+                      position: "absolute",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      border: "1px solid #ccc",
+                      borderRadius: "5px",
+                    }}
+                  >
                     <Spinner size="large" />
                   </div>
                   <iframe
                     src={video.link}
                     frameBorder="0"
                     allowFullScreen
-                    style={{ width: "100%", height: "100%", zIndex: 2, position: "relative" }}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      zIndex: 2,
+                      position: "relative",
+                    }}
                   ></iframe>
                 </Box>
               </HorizontalGrid>

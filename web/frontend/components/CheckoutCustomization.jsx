57d42825dc3ab6bc90b3b9e54a0537415167d2ee
Hujjat Nazari <Hujjat@users.noreply.github.com>
2023-10-12 09:58:34 +1100

    alignment issue fixed on cusotmisation page

--- web/frontend/components/CheckoutCustomization.jsx
+++ web/frontend/components/CheckoutCustomization.jsx
@@ -25,23 +25,23 @@ export function CheckoutCustomization({activeProfile = null, handleDataChange}) 
         <div style={{
             padding: '2rem 1rem',
             }}>
-            <HorizontalStack gap={{ xs: "2", sm: "4" }}>
+            <Grid columns={{ xs: 1, sm: 1, md: 1, lg: 1, xl: 1 }} gap={4}>
+
                 <Grid columns={{ xs: 1, sm: 3, md: 3, lg: 3, xl: 3 }} gap={2}>
                     
                     <Grid.Cell gap="2" columnSpan={{xs: 3, sm: 3, md: 1, lg: 1, xl: 1 }}>
                         <VerticalStack gap="2">
                             <Text as="h3" variant="headingMd">
-                               Checkout header
+                               Global customization
                             </Text>
 
                             <Text as='p' variant="bodyMd">
-                                A way of making the checkout page look nice and easy to use. You can change the colors, fonts, and shapes of the things on the page.
+                                Chnage global customization for your checkout page.
                             </Text>
                         </VerticalStack>
                     </Grid.Cell>
 
-                    <Grid.Cell columnSpan={{xs: 3, sm: 3, md: 2, lg: 2, xl: 2}}>
-
+                    <Grid.Cell columnSpan={{xs: 3, sm: 3, md: 2, lg: 2, xl: 2 }}>
                         <AlphaCard title="Orders" sectioned>
                         <Select
                             label="Alignment"
@@ -70,7 +70,52 @@ export function CheckoutCustomization({activeProfile = null, handleDataChange}) 
                     </Grid.Cell>
                     
                 </Grid>
-            </HorizontalStack>
+            
+                <Grid columns={{ xs: 1, sm: 3, md: 3, lg: 3, xl: 3 }} gap={2}>
+                    
+                    <Grid.Cell gap="2" columnSpan={{xs: 3, sm: 3, md: 1, lg: 1, xl: 1 }}>
+                        <VerticalStack gap="2">
+                            <Text as="h3" variant="headingMd">
+                               Checkout header
+                            </Text>
+
+                            <Text as='p' variant="bodyMd">
+                                A way of making the checkout page look nice and easy to use. You can change the colors, fonts, and shapes of the things on the page.
+                            </Text>
+                        </VerticalStack>
+                    </Grid.Cell>
+
+                    <Grid.Cell columnSpan={{xs: 3, sm: 3, md: 2, lg: 2, xl: 2}}>
+                        <AlphaCard title="Orders" sectioned>
+                        <Select
+                            label="Alignment"
+                            options={[
+                                { label: "Default", value: null },
+                                { label: "Start", value: "START" },
+                                { label: "Center", value: "CENTER" },
+                                { label: "End", value: "END" },
+                            ]}
+                            selected={activeProfile?.customizations?.header?.alignment}
+                            onChange={(value) => {
+                                const temp = activeProfile;
+                                temp.customizations = {
+                                    ...temp?.customizations,
+                                    header: {
+                                    ...temp?.customizations?.header,
+                                    alignment: value,
+                                    },
+                                };
+                                handleDataChange(temp); 
+                            }}
+                            value={activeProfile?.customizations?.header?.alignment}
+                        />
+                           
+                        </AlphaCard>
+                    </Grid.Cell>
+                    
+                </Grid>
+            
+            </Grid>
         </div>
        
     );

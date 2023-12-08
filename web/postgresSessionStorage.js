64b41e29dc8ece28c0d7356bd04043908de02ce3
Mahdi Akbary <mahdi.akbary.nikzad@gmail.com>
2023-12-08 12:01:06 +0430

    adds custom posgres

---
+++ web/postgresSessionStorage.js
@@ -0,0 +1,205 @@
+import fs from 'fs';
+import { Session } from '@shopify/shopify-api';
+
+import pgPromise from "pg-promise"
+
+const pgp = pgPromise({/* Initialization Options */ });
+// const DB = pgp('postgres://postgres:password@localhost:5433/test')
+const DB = pgp('postgres://default:oAqP7JpmdXc5@ep-jolly-voice-76934282.us-east-1.postgres.vercel-storage.com:5432/verceldb')
+
+export class PostgresSessionStorage {
+    filename = ""
+    constructor() {
+        this.init();
+    }
+
+    async storeSession (session) {
+        try {
+
+
+
+                console.log('---------------- adding session --------------', session)
+                // await DB.none(`
+                //     INSERT INTO shopify_sessions(
+                //         shop, 
+                //         state, 
+                //         is_online,
+                //         expires,
+                //         scope,
+                //         access_token,
+                //         online_access_info
+                //     ) VALUES(
+                //         ${session.shop},
+                //         ${session.state},
+                //         ${session.isOnline},
+                //         ${11},
+                //         ${session.scope},
+                //         ${session.accessToken},
+                //         ${'someting'}
+                // `);
+                
+
+                await DB.none('INSERT INTO shopify_sessions(shop, state, is_online, scope, access_token) VALUES( ${shop}, ${state}, ${isOnline}, ${scope}, ${accessToken})', session);
+
+
+        } catch (err) {
+            // error on read
+            console.log(err)
+            return false;
+        }
+    }
+
+    async loadSession (id) {
+        try {
+            const lines = this.readLines();
+
+            // process each line
+            for (const line of lines) {
+                // split the line by comma into columns
+                const columns = line.split(',');
+                // check if the id column matches (second column is value of 'id')
+                if (columns[1] === id) {
+                    // if the session id already exists, convert to session and return
+                    return Session.fromPropertyArray(
+                        this.columnsToPropertyArray(columns),
+                    );
+                }
+            }
+
+            return undefined;
+        } catch (err) {
+            // error on read
+            return undefined;
+        }
+    }
+
+    async deleteSession (id) {
+        try {
+            const lines = this.readLines();
+
+            // process each line
+            for (let i = 0; i < lines.length; i++) {
+                // split the line by comma into columns
+                const columns = lines[i].split(',');
+                // check if the id column matches (second column is value of 'id')
+                if (columns[1] === id) {
+                    // if the session id matches, remove the line
+                    lines.splice(i, 1);
+                    break;
+                }
+            }
+
+            try {
+                this.writeLines(lines);
+                return true;
+            } catch (err) {
+                // error on write
+                return false;
+            }
+        } catch (err) {
+            // error on read
+            return false;
+        }
+    }
+
+    async deleteSessions (ids) {
+        try {
+            const lines = this.readLines();
+
+            // process each line
+            for (let i = 0; i < lines.length; i++) {
+                // split the line by comma into columns
+                const columns = lines[i].split(',');
+                // check if the id column matches (second column is value of 'id')
+                if (ids.includes(columns[1])) {
+                    // if the session id already exists, remove the line
+                    lines.splice(i, 1);
+                }
+            }
+
+            try {
+                this.writeLines(lines);
+                return true;
+            } catch (err) {
+                // error on write
+                return false;
+            }
+        } catch (err) {
+            // error on read
+            return false;
+        }
+    }
+
+    async findSessionsByShop (shop) {
+        try {
+            const lines = this.readLines();
+
+            const sessions = [];
+
+            // process each line
+            for (const line of lines) {
+                // split the line by comma into columns
+                const columns = line.split(',');
+                // check if the shop column matches (fourth column is value of 'shop')
+                if (columns[3] === shop) {
+                    // if the shop matches, convert to session and add to array
+                    sessions.push(
+                        Session.fromPropertyArray(this.columnsToPropertyArray(columns)),
+                    );
+                }
+            }
+
+            return sessions;
+        } catch (err) {
+            // error on read
+            return [];
+        }
+    }
+
+    init () {
+        // if (!fs.existsSync(this.filename)) {
+        //     fs.writeFileSync(this.filename, '');
+        // }
+
+
+        const createSessionTableIfExistsSQL = `CREATE TABLE IF NOT EXISTS shopify_sessions (
+            id serial PRIMARY KEY,
+            shop VARCHAR ( 255 ) NOT NULL,
+            state VARCHAR ( 255 ) NOT NULL,
+            is_online BOOLEAN NOT NULL,
+            expires INT ,
+            scope VARCHAR ( 255 ) NOT NULL,
+            access_token VARCHAR ( 255 ) NOT NULL,
+            online_access_info VARCHAR ( 255 )
+        );`
+        DB.any(createSessionTableIfExistsSQL)
+
+    }
+
+    readLines () {
+        // read contents of the file
+        const data = fs.readFileSync(this.filename, {
+            encoding: 'utf8',
+            flag: 'r',
+        });
+
+        // split the contents by new line
+        return data.split(/\r?\n/);
+    }
+
+    writeLines (lines) {
+        // write the lines back to the file
+        fs.writeFileSync(this.filename, lines.join('\n'), {
+            encoding: 'utf8',
+            flag: 'w',
+        });
+    }
+
+    columnsToPropertyArray (columns) {
+        const propertyArray = [];
+        for (let i = 0; i < columns.length; i += 2) {
+            propertyArray.push([columns[i], columns[i + 1]]);
+        }
+        return propertyArray;
+    }
+}
--- web/shopify.js
+++ web/shopify.js
@@ -1,12 +1,12 @@
 import { BillingInterval, LATEST_API_VERSION, ApiVersion } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-import {PostgreSQLSessionStorage} from '@shopify/shopify-app-session-storage-postgresql';
 
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
 const DB_PATH = `${process.cwd()}/db.sqlite`;
 
 import { getBillingConfig } from "./billing.js"
+import { PostgresSessionStorage } from "./postgresSessionStorage.js";
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
@@ -27,9 +27,7 @@ const shopify = shopifyApp({
     path: "/api/webhooks",
   },
   // This should be replaced with your preferred storage strategy
-  sessionStorage: new PostgreSQLSessionStorage(
-    'postgres://default:oAqP7JpmdXc5@ep-jolly-voice-76934282.us-east-1.postgres.vercel-storage.com:5432/verceldb?ssl={"rejectUnauthorized":true}',
-  ),
+  sessionStorage: new PostgresSessionStorage()
 });
 
 export default shopify;

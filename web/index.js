49b361e380040a12b3f33c8071518b8911e4d44f
Mahdi Akbary <mahdi.akbary.nikzad@gmail.com>
2023-12-08 21:20:08 +0430

    updates the route

--- web/index.js
+++ web/index.js
@@ -24,8 +24,8 @@ console.log('watch here: ', process.cwd());
 
 const STATIC_PATH =
   process.env.NODE_ENV === "production"
-    ? `${process.cwd()}/frontend/dist`
-    : `${process.cwd()}/frontend/`
+    ? `./frontend/dist`
+    : `./frontend/`
 
 
 export const app = express()

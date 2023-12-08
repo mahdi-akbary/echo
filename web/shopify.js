9c6954203e0b01f620735661b477ded412cdb989
Mahdi Akbary <mahdi.akbary.nikzad@gmail.com>
2023-12-08 09:31:57 +0430

    adds postgres for vercel

--- web/shopify.js
+++ web/shopify.js
@@ -1,8 +1,10 @@
 import { BillingInterval, LATEST_API_VERSION, ApiVersion } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+import {PostgreSQLSessionStorage} from '@shopify/shopify-app-session-storage-postgresql';
+
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+const DB_PATH = `${process.cwd()}/db.sqlite`;
 
 import { getBillingConfig } from "./billing.js"
 
@@ -25,7 +27,9 @@ const shopify = shopifyApp({
     path: "/api/webhooks",
   },
   // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: new PostgreSQLSessionStorage(
+    'postgres://default:oAqP7JpmdXc5@ep-jolly-voice-76934282.us-east-1.postgres.vercel-storage.com:5432/verceldb',
+  ),
 });
 
 export default shopify;
